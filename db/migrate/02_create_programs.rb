f3acbbe972eb7134300ab76dffede35618cfbf0f
zenglue <keonilrath@gmail.com>
2016-11-07 02:51:24 -0500

    table info added

---
+++ db/migrate/02_create_programs.rb
@@ -0,0 +1,10 @@
+class CreatePrograms < ActiveRecord::Base
+  def change
+    create_table :programs do |t|
+      t.string :organization_name
+      t.string :url
+      t.string :address
+      t.string :telephone_number
+    end
+  end
+end
