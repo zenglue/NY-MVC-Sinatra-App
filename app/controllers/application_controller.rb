01bc055d34e91a2a886bf6c4e0302992a4973bc0
zenglue <keonilrath@gmail.com>
2016-11-07 01:56:07 -0500

    init route, helpers added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,18 @@ class ApplicaionController < Sinatra::Base
     enable :sessions
     set :session_secret, 'password_security'
   end
+
+  get '/' do
+    erb :"home"
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:id]
+    end
+
+    def current_user
+      User.find(session[:id])
+    end
+  end
 end
