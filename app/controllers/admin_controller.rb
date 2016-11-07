528e9dc15ef9075efe324f1830fde7edc9abf7f8
zenglue <keonilrath@gmail.com>
2016-11-07 02:08:24 -0500

    new controller, helper methods moved

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,12 @@
+class AdminController < ApplicationController
+
+  helpers do
+    def logged_in?
+      !!session[:id]
+    end
+
+    def current_user
+      User.find(session[:id])
+    end
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,14 +11,4 @@ class ApplicaionController < Sinatra::Base
   get '/' do
     erb :"home"
   end
-
-  helpers do
-    def logged_in?
-      !!session[:id]
-    end
-
-    def current_user
-      User.find(session[:id])
-    end
-  end
 end
