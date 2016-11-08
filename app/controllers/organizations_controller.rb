5496916cbde6c0ecfe7ebd3a9b11ab1d8230a76f
zenglue <keonilrath@gmail.com>
2016-11-07 22:53:47 -0500

    working on org/index route/view

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,4 +1,10 @@
 class OrganizationController < ApplicationController
+
+  get 'organizations/index' do
+    @organization = Organization.all
+    erb :"/organizations/index"
+  end
+  
   get 'organizations/:id' do
     @organization = Organization.find(params[:id])
     erb :"/organizations/show"
