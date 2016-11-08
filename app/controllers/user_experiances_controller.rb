a6c5441daec7800d4ccfa2e4b63d34be71b22033
zenglue <keonilrath@gmail.com>
2016-11-08 01:47:31 -0500

    experiance edit route

--- app/controllers/user_experiances_controller.rb
+++ app/controllers/user_experiances_controller.rb
@@ -4,14 +4,29 @@ class UserExperiancesController < ApplicationController
     if logged_in?
       erb :"/user_experiances/new"
     else
+#add session error message
       redirect '/users/login'
     end
   end
 
   get '/experiances/:id' do
     if logged_in?
-    @experiance = UserExperiance.find(params[:id])
-    erb :"/user_experiances/show"
+      @experiance = UserExperiance.find(params[:id])
+      erb :"/user_experiances/show"
+    else
+#add session error message
+      redirect '/users/login'
   end
+
+  get '/experiances/:id/edit' do
+    @experiance = UserExperiance.find(params[:id])
+    if logged_in? && @experiance.user_id == current_user.id
+      erb :"/user_experiances/edit"
+    else
+#add session error message
+      redirect '/users/login'
+    end
+  end
+
 
 end
