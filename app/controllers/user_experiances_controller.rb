6f36aaae984503fb9cf6237f08464cd4098e4f40
zenglue <keonilrath@gmail.com>
2016-11-08 01:13:17 -0500

    route to new experiance

--- app/controllers/user_experiances_controller.rb
+++ app/controllers/user_experiances_controller.rb
@@ -1,9 +1,17 @@
 class UserExperiancesController < ApplicationController
 
+  get '/experiances/new' do
+    if logged_in?
+      erb :"/user_experiances/new"
+    else
+      redirect '/users/login'
+    end
+  end
+
   get '/experiances/:id' do
+    if logged_in?
     @experiance = UserExperiance.find(params[:id])
     erb :"/user_experiances/show"
   end
 
-  end
 end
