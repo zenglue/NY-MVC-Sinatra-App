2d270283f9ba0ab4282f39a77df63429b7d0be0b
zenglue <keonilrath@gmail.com>
2016-11-09 02:32:09 -0500

    slug method added

--- app/models/users.rb
+++ app/models/users.rb
@@ -4,8 +4,10 @@ class User < ActiveRecord::Base
   # has_many :organization_experiances, through: :user_experiances
 
   def slug
+    self.username.strip.downcase.gsub(" ", "-")
   end
 
-  def self.find_by_slug
+  def self.find_by_slug(slug)
+    self.all.find{|user| user.slug == slug}
   end
 end
