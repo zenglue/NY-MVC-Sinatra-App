0d30f7a1867419105b89c6dcd12ac0dde2589f39
zenglue <keonilrath@gmail.com>
2016-11-08 21:07:09 -0500

    init seed data for orgs

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -6,9 +6,31 @@ class OrganizationController < ApplicationController
   end
 
   get '/organizations/:id' do
+
     @organization = Organization.find(params[:id])
-#set organization id to new experiance organizaition id
-#set link to create new experiance in view 
+
+
     erb :"/organizations/show"
   end
+
+  get '/organization/:id/new' do
+    if logged_in?
+
+      @organization = Organization.find(params[:id])
+      org_experiance = OrganizationExperiance.new
+      org_experiance.organization_id = @organization.id
+      org_experiance.user_id = current_user.id
+      org_experiance.save
+    #set link to create new experiance/ratings in view
+      redirect '/experiances/new'
+    else
+      # flash notice[:session] = "please login to record a new experiance"
+      redirect '/users/login'
+    end
+
+
+
+
+
+
 end
